323d9e261e25b2af6ad9c0ac0a765c72de3db343
Anatolii Bedilo <fides2013ml@gmail.com>
2022-04-22 21:39:42 +0300

    modifided mobile version menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 document.getElementById('burger-menu').onclick = (event) => {
   document.getElementById('burger-menu').classList.toggle('open')
   document.getElementById('main-menu').classList.toggle('active')
-  // document.getElementsByTagName('body')[0].classList.toggle('hidden')
+  document.getElementsByTagName('body')[0].classList.toggle('hidden')
 }
 
-//Scroll+fixed menu
-const nav = document.getElementById('menu-stiky')
-
-function checkMarginToTop () {
-
-  window.pageYOffset > nav.offsetTop
-    ? nav.classList.add('sticky-menu')
-    : nav.classList.remove('sticky-menuy')
-}
-
-window.onscroll = (event) => checkMarginToTop()
 
 //More item
 const btnMoreItem = document.querySelector('.more-item')
